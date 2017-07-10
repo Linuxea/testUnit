1441bfb958ff0749a159a5a3e730d4791dceaa73
linuxea.lin <linuxea.lin@Zu-PC63.bamboonetworks.com>
2017-07-10 16:14:11 +0800

    unit init！

---
+++ testUnit/src/main/java/testUnit/algorithm/CountWord.java
@@ -0,0 +1,50 @@
+package testUnit.algorithm;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * 
+ * @author Linuxea
+ * @version 1.0
+ */
+//遍历一个字符串，统计出现字母的个数
+public class CountWord {
+	
+	public static void main(String[] args) {
+		
+		String str;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入一串字符串便于统计:");
+		str = sc.nextLine();
+		
+		char[] ch = str.toCharArray();//将字符串->字符数组
+		
+		Map<Character,Integer> maps = new HashMap<>();
+		
+		for(int i = 0;i<ch.length;i++){
+			if(maps.get(ch[i])==null){
+				maps.put(ch[i], 1);
+			}else{
+				maps.put(ch[i],maps.get(ch[i])+1);
+			}
+		}
+		
+//System.out.println("看这里"+maps);
+			
+			Set<Character> sets = maps.keySet();
+			Iterator<Character> iter = sets.iterator();
+			
+			for(;iter.hasNext();){
+				char temp = iter.next();
+				System.out.println("字母:"+temp+"出现"+maps.get(temp));
+			}
+			sc.close();
+			sc = null;
+	}
+}
+
+
---
+++ testUnit/src/main/java/testUnit/algorithm/PowerOfFour.java
@@ -0,0 +1,22 @@
+package testUnit.algorithm;
+
+/**
+ * @author Linuxea
+ * @date 2016年7月29日 上午1:19:27
+ * @version 1.0
+ * @description Given an integer (signed 32 bits), write a function to check
+ *              whether it is a power of 4.
+ **/
+public class PowerOfFour {
+	public boolean isPowerOfFour(int num) {
+		
+		
+		return false;
+		
+	}
+	public static void main(String[] args) {
+		if(new PowerOfFour().isPowerOfFour(16)){
+			System.out.println("yes");
+		}
+	}
+}
