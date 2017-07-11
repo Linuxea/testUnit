e5c57f5172c7db62c64366b83aeb1a984bf5372e
linuxea.lin <linuxea.lin@Zu-PC63.bamboonetworks.com>
2017-07-11 10:33:31 +0800

    继承调用

---
+++ testUnit/src/main/java/basic/inherit/GrandFather.java
@@ -0,0 +1,17 @@
+package basic.inherit;
+
+/**
+ *@date      2017年7月11日 上午10:26:01
+ *@desc      爷爷类
+ *@version   1.0
+ *@author    Linuxea.lin
+*/
+public class GrandFather implements Person{
+
+	@Override
+	public void say() {
+		System.out.println("我是爷爷");
+	}
+	
+
+}
