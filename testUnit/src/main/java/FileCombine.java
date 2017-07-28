86aced3dec65cf11a9696c3bc86a56a869981230
linuxea <linuxea@foxmail.com>
2017-07-28 08:15:22 +0800

    文件分割与合并

---
+++ testUnit/src/main/java/FileCombine.java
@@ -0,0 +1,55 @@
+package reflect.aop;
+
+import org.testng.collections.Lists;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by Linuxea on 2017-07-28.
+ */
+public class FileCombine {
+
+    private static String baseFilePath = "C:\\Users\\Administrator.SKY-20170312TVX\\Desktop\\split";
+
+    public static void combine(String fileName) throws Exception {
+        File f = new File(baseFilePath);
+        File[] childs = f.listFiles(file -> {return file.getName().startsWith(fileName);});
+
+        List<File> fileList = Lists.newArrayList(childs);
+        fileList.sort(Comparator.comparing(File::getName));
+
+        List<byte[]> byteList = Lists.newArrayList(); //集成所有字节
+
+        for(File temp : fileList){
+            byte[] b = new byte[1024];
+            FileInputStream is = new FileInputStream(temp);
+            int byteCount = 0;
+            while(byteCount != -1){
+                byteCount =  is.read(b);
+                byteList.add(b);
+            }
+        }
+
+        File file = new File(baseFilePath + "\\" + "fuck.jpg");
+        for(byte[] bytes : byteList){
+            FileOutputStream out = new FileOutputStream(file);
+            out.write(bytes);
+            System.out.println("wrinting...");
+        }
+
+
+    }
+
+    public static void main(String[] argc){
+        try {
+            FileCombine.combine("d299399b-88f9-4d60-b813-c712ab7fb208_the");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ testUnit/src/main/java/FileSplit.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.*;
+
+/**
+ * Created by Linuxea on 2017-07-28.
+ */
+public class FileSplit {
+
+    private static final int size =20000; //切割文件大小
+    private int partNum; //切割块数  //TODO 根据块数去决定
+    private static String baseFilePath = "C:\\Users\\Administrator.SKY-20170312TVX\\Desktop\\split";
+
+    FileSplit(int partNum, String baseFilePath){
+        this.partNum = partNum;
+        this.baseFilePath = baseFilePath;
+    }
+
+    FileSplit(int partNum){
+        this.partNum = partNum;
+    }
+
+    public static void make(String fileName) throws Exception {
+
+        Map<String,byte[]> byteMap = new HashMap<>();
+        int count = 0; //切割的第N块
+        File file = new File(fileName);
+        if(!file.exists()){
+            System.out.println("file not exists");
+            return;
+        }
+
+        FileInputStream is = new FileInputStream(file);
+        int temp = 0;
+        String randomName = UUID.randomUUID().toString();
+        while(-1 != temp){
+            byte[] b = new byte[size];
+            temp =  is.read(b);
+            byteMap.put(randomName + "_the_" + (count++),b);
+            System.out.println("reading...");
+        }
+
+        //将数组存放在硬盘
+        Set<String> keys =  byteMap.keySet();
+        for(String keyStr : keys){
+            File f = new File(baseFilePath + "\\" + keyStr);
+            if(!f.exists())f.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(f);
+            outputStream.write(byteMap.get(keyStr));
+            System.out.println("writing ....");
+        }
+    }
+
+    public static void main(String[] argc) {
+        try {
+            FileSplit.make("C:\\Users\\Administrator.SKY-20170312TVX\\Desktop\\1.jpg");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
