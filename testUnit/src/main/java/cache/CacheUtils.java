d2706d2f84d0cbfa9dc625390003c7fbea69303c
linuxea <linuxea@foxmail.com>
2017-08-07 17:33:37 +0800

    简化

--- testUnit/src/main/java/cache/CacheUtils.java
+++ testUnit/src/main/java/cache/CacheUtils.java
@@ -87,8 +87,7 @@ public class CacheUtils {
 	 */
 	public static <T> T get(String key){
 		CacheObject thisCa = cache.get(key);
-		if(null == thisCa)return null;
-		if(thisCa.getExpireTime()<=0 || thisCa.getExpireTime()>System.currentTimeMillis()){
+		if(null == thisCa && (thisCa.getExpireTime()<=0 || thisCa.getExpireTime()>System.currentTimeMillis())){
 			return (T) thisCa.getValue();
 		}
 		//过时级别的缓存不在此处做销毁处理  以免销毁动作影响缓存带来的性能提升
@@ -133,8 +132,7 @@ public class CacheUtils {
 	 */
 	public static <T> T hget(String key, String field){
 		CacheObject obj = cache.get(key + ":" + field);
-		if(null == obj)return null;
-		if(obj.getExpireTime()<0 || obj.getExpireTime()>System.currentTimeMillis()){
+		if(null == obj && (obj.getExpireTime()<0 || obj.getExpireTime()>System.currentTimeMillis())){
 			return (T) obj.getValue();
 		}
 		return null;
