653f2892f1b58a2afc03ee00991d1d5a12edc4e2
linuxea <linuxea@foxmail.com>
2017-08-07 17:24:07 +0800

    add hello

---
+++ testUnit/src/main/java/cache/Hello.java
@@ -0,0 +1,14 @@
+package cache;
+
+import org.junit.Test;
+
+/**
+ * Created by Linuxea on 2017/8/7.
+ */
+public class Hello {
+
+	@Test
+	public void test(){
+		System.out.println("hello");
+	}
+}
