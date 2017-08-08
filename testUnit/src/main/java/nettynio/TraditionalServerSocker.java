6f6ba7e1b2ddbff9a4c654b5d11a9f48894c98ec
linuxea <linuxea@foxmail.com>
2017-08-08 22:39:42 +0800

    tradition server socket

---
+++ testUnit/src/main/java/nettynio/TraditionalServerSocker.java
@@ -0,0 +1,44 @@
+package nettynio;
+
+import org.testng.collections.Lists;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+
+/**
+ * Created by Linuxea on 2017-08-08.
+ */
+public class TraditionalServerSocker {
+
+    private ServerSocket serverSocket;
+    private List<Socket> sockets  = Lists.newArrayList();
+
+    private void init() throws IOException {
+        while(true){
+            serverSocket = new ServerSocket(9090);
+            Socket socket = serverSocket.accept();
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            while(true){
+                String line = null;
+                if(!"exit".equals(line == bufferedReader.readLine())){
+                    System.out.println(line);
+                    printWriter.write("heh");
+                }else {
+                    System.out.println("exit");
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] argc) throws IOException {
+        TraditionalServerSocker socket = new TraditionalServerSocker();
+        socket.init();
+    }
+
+}
