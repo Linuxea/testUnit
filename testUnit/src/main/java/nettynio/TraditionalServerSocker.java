668c442ef81b86f2906cbb86921d8a6ccb3216df
linuxea <linuxea@foxmail.com>
2017-08-09 01:22:10 +0800

    traditional server socket 多线程版

--- testUnit/src/main/java/nettynio/TraditionalServerSocker.java
+++ testUnit/src/main/java/nettynio/TraditionalServerSocker.java
@@ -1,11 +1,11 @@
 package nettynio;
 
+import jdk.internal.org.objectweb.asm.Handle;
 import org.testng.collections.Lists;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
 
 /**
  * Created by Linuxea on 2017-08-08.
@@ -13,36 +13,54 @@ import java.util.List;
 public class TraditionalServerSocker {
 
     private ServerSocket serverSocket;
-    private List<Socket> sockets  = Lists.newArrayList();
 
     private void init() throws IOException {
         serverSocket = new ServerSocket(9090);
         while(true){
             Socket socket = serverSocket.accept();
-            sockets.add(socket);
+            System.out.println("coming a new guy:" + socket.getRemoteSocketAddress());
+            new Thread(new Handle(socket)).start();
         }
     }
 
-    public void listen() throws Exception{
-        for(Socket socket : sockets){
-            while (true){
-               try(InputStream inputStream = socket.getInputStream();
-                   OutputStream outputStream = socket.getOutputStream();
-                   BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                   PrintWriter printWriter = new PrintWriter(outputStream)){
-                    while(true){
-                       String line = null;
-                       if(!"exit".equals(line == bufferedReader.readLine())){
-                           System.out.println(line);
-                           printWriter.write("heh");
-                       }else {
-                           System.out.println("exit");
-                       }
-                   }
-               }
+    private static class Handle implements  Runnable{
+
+        private BufferedReader bufferedReader=null;
+        private PrintWriter printWriter = null;
+        private String line;
+        private Socket socket;
+        public Handle(Socket socket){
+            this.socket = socket;
+        }
+
+        @Override
+        public void run(){
+            try {
+                bufferedReader= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                printWriter = new PrintWriter(socket.getOutputStream());
+                while (true){
+                    line = bufferedReader.readLine();
+                    if(null != line){
+                        System.out.println("received:" + line);
+                        printWriter.write("return:" + line.getBytes());
+                        printWriter.write("\n");
+                        printWriter.flush();
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }finally {
+                try {
+                    printWriter.close();
+                    bufferedReader.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
             }
+
         }
     }
+
 
 
     public static void main(String[] argc) throws IOException {
