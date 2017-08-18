dde3f2643eca5b964358229a627e2609a39a42a5
linuxea <linuxea@foxmail.com>
2017-08-18 17:13:23 +0800

    字符集的东东

--- testUnit/src/main/java/guava/string/StringTest.java
+++ testUnit/src/main/java/guava/string/StringTest.java
@@ -1,9 +1,14 @@
 package guava.string;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import org.junit.Test;
 import org.testng.collections.Sets;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -22,6 +27,31 @@ public class StringTest {
 		stringSet.add(null);
 		String joinerResult = Joiner.on(",").skipNulls().join(stringSet);
 		System.out.println(joinerResult);
+	}
+
+	@Test
+	public void splitterTest(){
+		String str = "a,b,c,d,e";
+		Iterable<String> iterable = Splitter.on(",").limit(3).split(str);
+		for(Iterator<String> iterator = iterable.iterator();iterator.hasNext();){
+			System.out.println(iterator.next());
+		}
+
+//		iterable.forEach(System.out::println);
+	}
+
+	@Test
+	public void charMatcherTest(){
+		String str = "11";
+		String newStr = CharMatcher.digit().and(CharMatcher.javaLowerCase()).removeFrom(str);
+		System.out.println(newStr);
+	}
+
+	@Test
+	public void StringByteTest(){
+		String str = "I love kimboo";
+		byte[] bytes = str.getBytes(Charsets.UTF_8);
+		System.out.println(Arrays.toString(bytes));
 	}
 
 }
