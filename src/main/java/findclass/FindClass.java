3b0442b5f07a8ee9b7c101ca2ac5e9332e8c87e9
linuxea <linuxea@foxmail.com>
2017-08-19 17:26:43 +0800

    add a class test classloader

---
+++ src/main/java/findclass/FindClass.java
@@ -0,0 +1,32 @@
+package findclass;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.net.URL;
+
+/**
+ * Created by Linuxea on 2017-08-19.
+ */
+public class FindClass {
+
+    @Test
+    public void test1(){
+        URL url =
+                this.getClass().getClassLoader().getResource("algorithm/easy");
+        System.out.println(url.getFile());
+    }
+
+    @Test
+    public void test2() throws IOException {
+        try(InputStream inputStream =
+                    this.getClass().getClassLoader().getResourceAsStream("algorithm/eas/.Lottery");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))){
+            String str;
+            while (null != (str = bufferedReader.readLine())){
+                System.out.println(str);
+            }
+        }
+    }
+
+}
