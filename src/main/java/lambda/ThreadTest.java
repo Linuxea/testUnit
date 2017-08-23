2dfcd9868ac565ea4bd67f7dafb95f1c5dd6e788
linuxea <linuxea@foxmail.com>
2017-08-23 13:34:01 +0800

    Thread test

---
+++ src/main/java/lambda/ThreadTest.java
@@ -0,0 +1,17 @@
+package lambda;
+
+import org.junit.Test;
+
+public class ThreadTest {
+
+    @Test
+    public void test1(){
+        new Thread(() -> System.out.println("hello")).start();
+    }
+
+    @Test
+    public void test2(){
+       Runnable runnable = () -> System.out.println("run..");
+       new Thread(runnable).start();
+    }
+}
