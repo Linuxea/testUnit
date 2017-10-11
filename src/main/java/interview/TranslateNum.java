fe2d5ca6edf08708d561b8b068a927188f92ba7b
Linuxea <Linuxea@foxmail.com>
2017-10-11 18:20:29 +0800

    强迫症
    细节注意问题

--- src/main/java/interview/TranslateNum.java
+++ src/main/java/interview/TranslateNum.java
@@ -11,6 +11,9 @@ import com.google.common.collect.Maps;
  * 因为特殊情况并不多，所以这种方式最简单 
  * 对输入数字进行预检测
  * 以及尾数“零”的处理
+ * 主要考察点在于细节上
+ * 难过 :)
+ *
  * @author linuxea.lin
  *
  */
@@ -21,9 +24,9 @@ public class TranslateNum {
 	public static void sc(int x){
 		init();
 		check(x);
-		if( x >= 0 && x <= 9){
+		if( x >= 0 && x <= 9 ){
 			System.out.println(map.get(x+""));
-		}else if(x>=10 && x <= 99){
+		}else if( x>=10 && x <= 99 ){
 			StringBuilder temp = new StringBuilder();
 			String[] strArr = String.valueOf(x).split("");
 			temp.append(map.get(strArr[0])).append("十");
@@ -52,24 +55,24 @@ public class TranslateNum {
 	private static String postCheck(StringBuilder temp) {
 		char[] arr = temp.toString().toCharArray();
 		int i = arr.length - 1 ;
-		for(; i>0; i--){
-			if(!Character.valueOf(arr[i]).toString().equals("零")){ // char vs string
-				break;
-			}
-		}
+		for(;i>0 && Character.valueOf(arr[i--]).toString().equals("零"););
 		return temp.subSequence(0, i+1).toString();
 	}
 
+	/**
+	 * 入参检查
+	 * 借助编译器
+	 * @param x
+	 */
 	private static void check(int x) {
-		if( x < 0  || x > 999){
+		if( x <= 0  || x > 999){
 			throw new IllegalArgumentException("数字必须在[1,999]");
 		}
 	}
 
 	private static void init() {
-		map.put(0+"", "零");
-		map.put(1+"", "一"); map.put(2+"", "二"); map.put(3+"", "三"); map.put(4+"", "四"); map.put(5+"", "五");
-		map.put(6+"", "六"); map.put(7+"", "七"); map.put(8+"", "八"); map.put(9+"", "九");
+		map.put(0+"", "零"); map.put(1+"", "一"); map.put(2+"", "二"); map.put(3+"", "三"); map.put(4+"", "四"); 
+		map.put(5+"", "五"); map.put(6+"", "六"); map.put(7+"", "七"); map.put(8+"", "八"); map.put(9+"", "九");
 	}
 	
 	public static void main(String[] args) {
