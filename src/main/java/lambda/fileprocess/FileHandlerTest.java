709a3edb75d0f63fe4dd4ca0546310d25ac2b93f
Linuxea <Linuxea@foxmail.com>
2017-10-16 10:45:23 +0800

    文件处理函数式测试

---
+++ src/main/java/lambda/fileprocess/FileHandlerTest.java
@@ -0,0 +1,32 @@
+package lambda.fileprocess;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+/**
+ * 文件处理函数式测试
+ * @author linuxea.lin
+ *
+ */
+public class FileHandlerTest {
+
+	
+	@Test
+	public void test() throws IOException{
+		
+		new FileHandler().fileProcess(br -> {
+			try{
+				StringBuilder sb = new StringBuilder(10);
+				sb.append(br.readLine());
+				sb.append("\n");
+				sb.append(br.readLine());
+				System.out.println(sb.toString());
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		
+	}
+	
+}
