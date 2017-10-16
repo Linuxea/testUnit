50459eb5e5caaba3c6e04cb77304e2ce4e0e7b64
Linuxea <Linuxea@foxmail.com>
2017-10-16 10:44:53 +0800

    文件处理函数式接口

---
+++ src/main/java/lambda/fileprocess/Process.java
@@ -0,0 +1,16 @@
+package lambda.fileprocess;
+
+import java.io.BufferedReader;
+
+
+/**
+ * 文件处理函数式接口
+ * @author linuxea.lin
+ *
+ */
+@FunctionalInterface
+public interface Process {
+	
+	void believe(BufferedReader br);
+
+}
