8f698cd7d90aada0e7bc41237ce96e5989a0ebb2
linuxea <5201314123abc>
2017-10-30 01:46:30 +0800

    jtime sample

---
+++ src/main/java/lambda/jtime/JDateTime.java
@@ -0,0 +1,31 @@
+package lambda.jtime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class JDateTime {
+
+    public static void init() {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+
+        String meDate = "2017-10-30";
+        DateTimeFormatter dateFormater = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate parseDate = LocalDate.parse(meDate, dateFormater);
+        System.out.println(parseDate);
+
+        String meDateTime = "2017-10-30 01:31:26";
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime parseDateTime = LocalDateTime.parse(meDateTime, dateTimeFormatter);
+        System.out.println(parseDateTime);
+    }
+
+    public static void main(String[] args) {
+        JDateTime.init();
+    }
+
+}
