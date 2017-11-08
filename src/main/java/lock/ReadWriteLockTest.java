d793a1521fe46fab79f63a07842841322ae2a4a6
Linuxea <linuxea@foxmail.com>
2017-11-09 00:10:23 +0800

    重入锁 ReenTrantLock

---
+++ src/main/java/lock/ReadWriteLockTest.java
@@ -0,0 +1,55 @@
+package lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReadWriteLockTest {
+
+    private ReentrantLock reentrantLock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        ReadWriteLockTest readWriteLockTest = new ReadWriteLockTest();
+        new Thread(() -> {
+            try {
+                readWriteLockTest.test();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "test-1").start();
+
+        new Thread(() -> {
+            try {
+                readWriteLockTest.test2();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "test-2").start();
+    }
+
+    public void test() throws InterruptedException {
+        ReentrantLock reentrantLock = this.reentrantLock;
+        Condition condition = reentrantLock.newCondition();
+        reentrantLock.lock();
+        System.out.println(Thread.currentThread().getName() + " lock");
+        try {
+            // await 将释释放锁  如果没有时间限制将一直
+            condition.await(5, TimeUnit.SECONDS);
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+
+    public void test2() throws InterruptedException {
+        ReentrantLock reentrantLock = this.reentrantLock;
+        Condition condition = reentrantLock.newCondition();
+        reentrantLock.lock();
+        System.out.println(Thread.currentThread().getName() + " lock");
+        try {
+            condition.await(5, TimeUnit.SECONDS);
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+
+}
