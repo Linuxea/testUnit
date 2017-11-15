2ed7e69973dcf8c0c92e2fd49ecc8de6f3f5a10b
Linuxea <linuxea@foxmail.com>
2017-11-15 21:43:59 +0800

    bit operation

---
+++ src/main/java/interview/yida/BitOperation.java
@@ -0,0 +1,16 @@
+package interview.yida;
+
+/**
+ * 位运算
+ */
+public class BitOperation {
+
+    public static void main(String[] args) {
+
+        System.out.println(-2 << 3); // 左移 ＊ 2^3
+
+        System.out.println(-2 >> 3); // 右移 / 2^3
+
+    }
+
+}
