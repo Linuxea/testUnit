31bc90e8a598d6e61b99a8c23bec5a8dd431e825
Linuxea <linuxea@foxmail.com>
2017-11-15 21:57:00 +0800

    variable override is not virtual

---
+++ src/main/java/interview/yida/overridefield/OverRideField.java
@@ -0,0 +1,28 @@
+package interview.yida.overridefield;
+
+public class OverRideField {
+
+    public static void main(String[] args) {
+        Son son = new Son();
+        son.print();
+    }
+
+}
+
+class Father {
+    String name = "father";
+
+    public void print() {
+        System.out.println(this); // why is son
+        System.out.println(this.name);  // why is not son's field name value ?
+    }
+
+}
+
+
+class Son extends Father {
+
+    String name = "son";
+}
+
+
