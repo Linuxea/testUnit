5759af67059068d7e58855922da3baccabee3cf6
Linuxea <linuxea@foxmail.com>
2017-11-23 15:29:36 +0800

    the importance of double check

---
+++ src/main/java/threads/MultiThreadEnter.java
@@ -0,0 +1,38 @@
+package threads;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * create by linuxea on 17-11-23
+ **/
+public class MultiThreadEnter {
+
+    private int i = 0;
+
+    public static void main(String[] args) {
+        MultiThreadEnter multiThreadEnter = new MultiThreadEnter();
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                try {
+                    multiThreadEnter.go();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }, "name:" + i).start();
+        }
+    }
+
+
+    public void go() throws InterruptedException {
+        if (i == 0) {
+            TimeUnit.SECONDS.sleep(2);
+            synchronized (this) {
+                if (i == 0) { // it is necessary double check
+                    ++i;
+                    System.out.println("no body can enter after me " + Thread.currentThread().getName());
+                }
+            }
+        }
+    }
+
+}
