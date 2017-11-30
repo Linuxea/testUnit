2e6be6f3f8c62f067be9f35ee6c56ce3abbc88ad
Linuxea <linuxea@foxmail.com>
2017-11-30 23:18:09 +0800

    obj <=> byte[]

---
+++ src/main/java/io/ObTest.java
@@ -0,0 +1,58 @@
+package io;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
+/**
+ * @author linuxea
+ * @date 17-11-30
+ **/
+public class ObTest {
+
+    static PiKaQ piKaQ = new PiKaQ(1, "me");
+
+    /**
+     * obj2byte
+     *
+     * @return
+     * @throws Exception
+     */
+    public static byte[] obj2By() throws Exception {
+        byte[] bytes;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(piKaQ);
+        oos.close();
+        bytes = baos.toByteArray();
+        baos.close();
+        System.out.println(Arrays.toString(bytes));
+        return bytes;
+    }
+
+    /**
+     * byte2obj
+     *
+     * @throws Exception
+     */
+    public static void by2Obj() throws Exception {
+        byte[] bytes = ObTest.obj2By();
+        Object ob;
+        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(bytes));
+        ob = ois.readObject();
+        ois.close();
+        System.out.println(ob);
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            ObTest.by2Obj();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/io/PiKaQ.java
@@ -0,0 +1,43 @@
+package io;
+
+import java.io.Serializable;
+
+/**
+ * @author linuxea
+ * @date 17-11-30
+ **/
+public class PiKaQ implements Serializable {
+
+    private int id;
+    private String name;
+
+    public PiKaQ(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PiKaQ{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
