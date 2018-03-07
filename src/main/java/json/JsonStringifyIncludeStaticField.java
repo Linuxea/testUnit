d15464ca8c97c7ceebe37a5b8da8300979dc9e49
Linuxea@foxmail.com <linuxea@foxmail.com>
2018-03-07 10:40:00 +0800

    json序列化添加static字段

---
+++ src/main/java/json/JsonStringifyIncludeStaticField.java
@@ -0,0 +1,38 @@
+package json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * @author linuxea
+ * @date 2018/3/7
+ */
+public class JsonStringifyIncludeStaticField {
+	
+	public static void main(String[] args) {
+		
+		ResponseStatus responseStatus = new ResponseStatus();
+		
+		Gson gson = new Gson();
+		String s = gson.toJson(responseStatus);
+		System.out.println(s);
+		
+		
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		
+		// Allowing the serialization of static fields
+		gsonBuilder.excludeFieldsWithModifiers(java.lang.reflect.Modifier.TRANSIENT);
+		// Creates a Gson instance based on the current configuration
+		Gson gsonIncludeStatic = gsonBuilder.create();
+		String json = gsonIncludeStatic.toJson(responseStatus);
+		System.out.println(json);
+		
+	}
+	
+	private static class ResponseStatus {
+		
+		public static final String go = "let's go";
+		public final int score = 1234;
+	}
+	
+}
