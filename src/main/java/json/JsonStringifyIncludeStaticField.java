b4d4fb7746b48a592a2816af9645e42dbe9b3e7b
Linuxea@foxmail.com <linuxea@foxmail.com>
2018-03-07 10:43:27 +0800

    add refer

--- src/main/java/json/JsonStringifyIncludeStaticField.java
+++ src/main/java/json/JsonStringifyIncludeStaticField.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 /**
+ *  refer https://stackoverflow.com/questions/14644860/why-static-fields-not-serialized-using-google-gson-gsonbuilder-json-parser
  * @author linuxea
  * @date 2018/3/7
  */
