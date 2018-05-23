7a1d4baa411772e4239fe353a06b2b6899bb7943
Linuxea@foxmail.com <linuxea@foxmail.com>
2018-05-23 21:11:02 +0800

    面向对象 this 困惑

--- src/main/java/interview/yida/overridefield/OverRideField2.java
+++ src/main/java/interview/yida/overridefield/OverRideField2.java
@@ -1,36 +1,47 @@
 package interview.yida.overridefield;
 
-public class OverRideField {
+/**
+ *
+ */
+public class OverRideField2 {
 	
 	public static void main(String[] args) {
-		Son son = new Son();
+		Son2 son = new Son2();
 		son.print();
 		son.printName();
 	}
 	
 }
 
-class Father {
+class Father2 {
 	
 	String name = "father";
 	
 	public void print() {
-		System.out.println(this);
 		// why is son
+		System.out.println(this);
 	}
 	
 	public void printName() {
 		// why is not son's field name value ?
-		System.out.println(this.name);
+		System.out.println(this.getName());
+	}
+	
+	public String getName() {
+		return "father";
 	}
 	
 }
 
 
-class Son extends Father {
+class Son2 extends Father2 {
 	
 	String name = "son";
 	
+	@Override
+	public String getName() {
+		return "son";
+	}
 }
 
 
