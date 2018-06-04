3bed47a537e4a22f9aab3d67a80415e13470c2cd
Linuxea@foxmail.com <linuxea@foxmail.com>
2018-06-05 01:30:25 +0800

    String fuckString = new String("abc");

---
+++ src/main/java/interview/jingyugame/StringObjEq.java
@@ -0,0 +1,23 @@
+package interview.jingyugame;
+
+/**
+ * @author linuxea
+ * @date 2018/6/5
+ */
+public class StringObjEq {
+	
+	public void run() {
+		String str = "Hello world";
+		String str2 = "Hello " + new String("world");
+		String str3 = "Hello " + "world";
+		System.out.println(str.equals(str2));
+		System.out.println(str == str3);
+		System.out.println(str == str2);
+		
+		// str 与 str2 是两个不一样的对象
+		// str 是创建在常量池中的一个 string 字面量 Hello world
+		// str2 是通过 stringBuilder 将两个单词拼接起来的新创建对象
+		// https://stackoverflow.com/questions/1881922/questions-about-javas-string-pool
+	}
+	
+}
---
+++ src/test/java/interview/jingyugame/StringObjEqTest.java
@@ -0,0 +1,15 @@
+package interview.jingyugame;
+
+import org.junit.Test;
+
+/**
+ * @author linuxea
+ * @date 2018/6/5
+ */
+public class StringObjEqTest {
+	
+	@Test
+	public void test() {
+		new StringObjEq().run();
+	}
+}
