02d5b90d1ca66b2e0d28930da644d17c726bbdca
Linuxea@foxmail.com <linuxea@foxmail.com>
2018-06-06 08:45:43 +0800

    c 与 java 中的同名局部变量的不同行为

---
+++ src/main/java/basic/SameNameLocalVariable.java
@@ -0,0 +1,17 @@
+package basic;
+
+/**
+ * @author linuxea
+ * @date 2018/6/6
+ */
+public class SameNameLocalVariable {
+
+//	public void run(){
+//		int i = 0;
+//		// 下面再声明一个 i 会报错 与c语言不同
+//		for(int i = 0;i<10;i++) {
+//			System.out.println(i);
+//		}
+//	}
+	
+}
